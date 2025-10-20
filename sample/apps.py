cf596cd38b4efa0f18f7283949173070d71e7abf
mashakim360 <mashakim360@gmail.com>
2025-10-19 19:53:38 -0700

    Refactor module

--- sample/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class SampleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sample'
-    enabled = True  # <-- flag untuk modul aktif
--- whatsapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class WhatsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'whatsapp'
-    enabled = True  # <-- flag untuk modul aktif
